cec36028291964cd4afaecd4ad143bc0f5cb2054
mimmeghla01 <mimmaaktermeghla2003@gmail.com>
2023-03-14 16:52:16 +0600

    first commit

---
+++ xpa1.js
@@ -0,0 +1,12 @@
+// make feet to inchs
+function feetToInch (inch){
+    let feet ="feet";
+    console.log(feet);
+    console.log("make inch:",inch);
+    return inchs;
+}
+
+
+var feet = 23;
+var inchs = feet*12;
+console.log(inchs);
---
+++ xpa2.js
@@ -0,0 +1,11 @@
+function centimeterToMeter(){
+    let centimeter = "centimeter";
+    console.log (centimeter);
+    console.log("make meter:",meter);
+    return result
+    
+}
+
+var centimeter = 234;
+var result = centimeter * 0.01 ;
+console.log(result);
